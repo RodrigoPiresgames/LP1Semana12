1134eb2feb4203023765b5361c7175d4e583dbab
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-06-02 13:36:10 +0100

    Add Class Checker

---
+++ HowManyOfThisType/Checker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Class1
+{
+    public static int HowManyOfThisType<T>(IEnumerable<object> items)
+    {
+
+        private int count;
+
+        foreach (object o in items)
+        {
+            if ( o is T)
+                count++;
+        }
+
+        return count++;
+    }
+
+}
